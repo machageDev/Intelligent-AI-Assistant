7be41d11bcfa8d68a61e153747bdea61b7b32e25
machageDev <machagefranklyn@gmail.com>
2026-05-12 22:49:54 +0300

    mplement ChatPage UI with BlocBuilder and typing indicator

--- akilichat/lib/screens/chat_pag.dart
+++ akilichat/lib/screens/chat_pag.dart
@@ -1,46 +1,68 @@
-import 'package:akilichat/bloc/chat_state.dart';
-import 'package:akilichat/bloc/chta_bloc.dart';
-import 'package:akilichat/widgets/chat_input.dart';
-import 'package:akilichat/widgets/message_bubble.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import '../bloc/chta_bloc.dart';
+import '../bloc/chat_event.dart';
+import '../bloc/chat_state.dart';
+import '../widgets/message_bubble.dart';
 
+class ChatPage extends StatefulWidget {
+  const ChatPage({super.key});
 
+  @override
+  State<ChatPage> createState() => _ChatPageState();
+}
 
-class ChatPage extends StatelessWidget {
-  ChatPage({super.key});
+class _ChatPageState extends State<ChatPage> {
+  final TextEditingController _controller = TextEditingController();
 
-  final TextEditingController controller =
-      TextEditingController();
+  void _send() {
+    if (_controller.text.isNotEmpty) {
+      context.read<ChatBloc>().add(SendMessageEvent(_controller.text));
+      _controller.clear();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("AI Assistant"),
-      ),
+      appBar: AppBar(title: const Text("Akilichat AI")),
       body: Column(
         children: [
           Expanded(
             child: BlocBuilder<ChatBloc, ChatState>(
               builder: (context, state) {
                 return ListView.builder(
+                  reverse: false, // Set to true if you want messages to start from bottom
                   padding: const EdgeInsets.all(16),
                   itemCount: state.messages.length,
                   itemBuilder: (context, index) {
-                    final message = state.messages[index];
-
-                    return MessageBubble(
-                      text: message.text,
-                      isUser: message.isUser,
-                    );
+                    final msg = state.messages[index];
+                    return MessageBubble(text: msg.text, isUser: msg.isUser);
                   },
                 );
               },
             ),
           ),
-
-          ChatInput(controller: controller),
+          if (context.watch<ChatBloc>().state.isTyping)
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: LinearProgressIndicator(),
+            ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _controller,
+                    decoration: const InputDecoration(hintText: "Type a message..."),
+                    onSubmitted: (_) => _send(),
+                  ),
+                ),
+                IconButton(icon: const Icon(Icons.send), onPressed: _send),
+              ],
+            ),
+          ),
         ],
       ),
     );
