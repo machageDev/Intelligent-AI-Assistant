099e98202276e2f0b61b020b28bdf73cf2f79d94
machageDev <machagefranklyn@gmail.com>
2026-05-22 14:33:46 +0300

    added service class

--- akilichat/lib/service/api_practice.dart
+++ akilichat/lib/service/api_practice.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:math';
 import 'package:akilichat/models/practice_model.dart';
 import 'package:http/http.dart' as http ;
 
@@ -24,7 +25,34 @@ class ProductService {
       throw Exception('network error occured:$e');
       }
     }
-  }
+    Future<ProductModel> createProduct(ProductModel newProduct) async {
+      final Url = Uri.parse('$_baseurl/products');
+
+      try{
+        final response = await http.post(
+          url,
+          headers:{
+            'Content-Type':'application/json',
+          },
+          body: json.encode(newProduct.toJson()),
+        );
+        if(response.statusCode == 201){
+          final Map<String,dynamic> decodedData = json.decode(response.body);
+          return ProductModel.fromJson(decodedData);
+
+        }else {
+          throw Exception('failed to save data:Server returned status ${response.statusCode}');
+
+        } 
+        catch (e) {
+          
+          throw Exception('Network error occured while saving data:$e');
+
+        }
+      }
+    }
+}   
+  
 
 
 
